5393e5bf2ee061a7053a19c7fe0e6296c82c9047
SONUKUMAR <sonukumarbksce1204@gmail.com>
2024-01-21 22:44:33 +0530

    Version 1

---
+++ part2/array/StringArray.java
@@ -0,0 +1,9 @@
+package part2.array;
+
+public class StringArray {
+    public static void main(String[] args){
+        String[] arr=new String[4];
+        System.out.println(arr[0]); //by default null
+
+    }
+}
