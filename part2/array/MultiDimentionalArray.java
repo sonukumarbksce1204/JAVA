bb0f18938540bc21df21fa686f5b8e7498bedaf7
SONUKUMAR <sonukumarbksce1204@gmail.com>
2024-01-23 23:49:53 +0530

    Version 1

---
+++ part2/array/MultiDimentionalArray.java
@@ -0,0 +1,21 @@
+package part2.array;
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class MultiDimentionalArray{
+    public static void main(String[] args){
+        /*
+                1 2 3
+                4 5 6
+                7 8 9      like a metrix
+        */
+
+        //int[][] arr=new int[3][]; //here [row][colums] and entry in row in compulsery not columns
+        int[][] arr= {
+                    {1,2,3},
+                    {4,5,6},
+                    {7,8,9}
+        };
+    }
+}
